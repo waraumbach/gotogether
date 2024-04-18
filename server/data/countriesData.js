0adb5417ce548fd9be8ff33397c5a687f89417a6
wara <144021938+waraumbach@users.noreply.github.com>
2024-04-18 17:56:17 +0200

    added data models

---
+++ server/data/countriesData.js
@@ -0,0 +1,45 @@
+const countriesData = [
+  {
+    id: 1,
+    name: "Japan",
+    region: "Asia",
+    reviews: ["4 good service"],
+    image: "/japan.jpg",
+  },
+  {
+    id: 2,
+    name: "Thailand",
+    region: "Asia",
+    reviews: ["4 good service"],
+    image: "/bangkok.jpg"
+  },
+  {
+    id: 3,
+    name: "South Korea",
+    region: "Middle East",
+    reviews: ["4 good service"],
+    image: "/korea.jpg"
+  },
+  {
+    id: 4,
+    name: "Malaysia",
+    region: "Asia",
+    reviews: ["4 good service"],
+    image: "/malaysia.jpg"
+  },
+  {
+    id: 5,
+    name: "Singapore",
+    region: "Asia",
+    reviews: ["4 good service"],
+    image: "/singapore.jpg"
+  },
+  {
+    id: 6,
+    name: "China",
+    region: "Asia",
+    reviews: ["4 good service"],
+    image: "/china.jpg"
+  },
+];
+export default countriesData;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 import express from 'express'
+import countriessData from "./data/countriesData.js";
+import cors from "cors";
 
 const app=express()
+
+//app.use(express.static("public"));
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
 
 app.get('/', (request,response)=>{
     response.send('welcome to countries playlist')
 })
-
+app.get("/countriesData", (req, res) => {
+  res.json(countriessData);
+});
+//access to data to id
+app.get("/countries/:id", (req, res) => {
+  let { id } = req.params;
+  const country = countrysData.find(
+    (country) => country.id === parseInt(id)
+  );
+  res.json(country);
+});
 
 
 app.listen(3000, () => console.log(`Server running on port 3000`))
