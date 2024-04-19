f79d4f469235ce6e6116233002251c0df66eeb32
wara <144021938+waraumbach@users.noreply.github.com>
2024-04-19 13:49:50 +0200

    added data

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import countriessData from "./data/countriesData.js";
+import countriesData from "./data/countriesData.js";
 import cors from "cors";
-//const URI =('mongodb+srv://Wara:KlLfg73LvsoaRfvS@cluster0.gobenur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+import mongoose from 'mongoose'
+const URI ='mongodb+srv://Wara:KlLfg73LvsoaRfvS@cluster0.gobenur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const app=express()
 // 1.access to localhost (httplocalhost) &postman 
 // GET : read the data
@@ -14,14 +15,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
 
-//mongoose.connect(URI)
+mongoose.connect(URI)
 
 
 app.get('/', (request,response)=>{
     response.send('welcome to countries playlist')
 })
-app.get("/countriesData", (req, res) => {
-  res.json(countriessData);
+app.get("/countries", (req, res) => {
+  res.json(countriesData);
 });
 //access to data to id
 app.get("/countries/:id", (req, res) => {
