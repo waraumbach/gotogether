33eb18a4dbcc11fc65442636b96de233fd60a9db
wara <144021938+waraumbach@users.noreply.github.com>
2024-04-23 13:29:20 +0200

    added more components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "../App.jsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppRouter from './AppRouter.jsx'
+import './index.css'
+import {BrowserRouter} from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).rend(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>,
 )
