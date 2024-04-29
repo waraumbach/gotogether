f0ed4e86f4ced5df47f30e4c325e99ced0e3684c
wara <144021938+waraumbach@users.noreply.github.com>
2024-04-29 16:33:59 +0200

    added footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer=()=> {
     return (
       <>
  
-<footer class="footer footer-center  w-full p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-gray-800">
+<footer class="footer footer-center w-full p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-gray-800">
       <div class="text-center">
         <p>Copyright © 2024 -<a class="font-semibold" href="mailto:gotogetherk@gmail.com">go together</a></p>
       </div>
